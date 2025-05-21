8cf417ef376816bc0b1adfbba04f68895413252c
Alexander Olsen <aolsen@ucsd.edu>
2025-05-21 16:58:43 -0700

    rename command listener file

--- src/events/commandListener.js
+++ src/events/commandListener.js
@@ -1,12 +1,13 @@
 const { Collection } = require('@discordjs/collection');
 const { Events } = require('discord.js');
 
+// processes and executes Chat Input Commands
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
 
-	
 		if (!interaction.isChatInputCommand()) return;
+
 		const command = interaction.client.commands.get(interaction.commandName);
 
 		// check if this is a found command for the system
@@ -28,14 +29,14 @@ module.exports = {
 		const cooldownAmount = (command.cooldown ?? defaultCooldownDur) * 1_000;
 
 		if (timestamps.has(interaction.user.id)) {
-			console.warn("[WARN]: Cooldown triggered");
+			console.warn('[WARN]: Cooldown triggered');
 
 			const expireTime = timestamps.get(interaction.user.id) + cooldownAmount;
 			if (now < expireTime) {
 				const expiredTimestamp = Math.round(expireTime / 1_000);
 				return interaction.reply({ content: `Please wait, you are still on cooldown until \`${expiredTimestamp}\``});
 			}
-			return interaction.reply("Still on cooldown!");
+			return interaction.reply('Still on cooldown!');
 		}
 
 		timestamps.set(interaction.user.id, now);	// set user id, remove after cooldown
@@ -43,7 +44,8 @@ module.exports = {
 
 		try {
 			await command.execute(interaction);
-		} catch (error) {
+		}
+		catch (error) {
 			console.error(error);
 			if (interaction.replied || interaction.deferred) {
 				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
